fabcd4a05c64d0ca6eef60f4920fb8166e373909
Zana H <zhertica@gmail.com>
2020-05-29 02:20:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,7 +2,7 @@ def game_hash
   {
     :home=>{
       :team_name=>"",
-      :colors=>[Black,White],
+      :colors=>["Black","White"],
       :players=>[{
         player_name: "Alan Anderson", number: 0, shoe: 16, points: 22, rebounds: 12, assists: 12, steals: 3, blocks: 1, slam_dunks: 1
       },{
@@ -17,7 +17,7 @@ def game_hash
     },
     :away=>{
       :team_name=>"Charlotte Hornets",
-      :colors=>[Turquoise,Purple],
+      :colors=>["Turquoise","Purple"],
       :players=>[{
          player_name: "Jeff Adrien", number: 4, shoe: 18, points: 10, rebounds: 1, assists: 1, steals: 2, blocks: 7, slam_dunks: 2
          },{
