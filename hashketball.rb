698757b62598b76835ea9fe966d1288a0ed135e6
Zana H <zhertica@gmail.com>
2020-05-29 01:51:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,15 +4,7 @@ def game_hash
       :team_name=>"Brooklyn Nets",
       :colors=>[Black, White],
       :players=>[{
-        player_name: ,
-        number: ,
-        shoe: ,
-        points: ,
-        rebounds: ,
-        assists: ,
-        steals: ,
-        blocks: ,
-        slam_dunks: 
+        
       }]
     },
     :away=>{
