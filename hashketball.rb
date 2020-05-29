612a301815b2e1c0f4d7c7433af19a64a7b83e3b
Zana H <zhertica@gmail.com>
2020-05-29 02:05:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,14 +5,14 @@ def game_hash
       :colors=>[],
       :players=>[{
         player_name: "Alan Anderson",
-        number: 0
-        shoe: 16
-        points: 
-        rebounds:
-        assists:
-        steals:
-        blocks:
-        slam_dunks:
+        number: 0,
+        shoe: 16,
+        points: 22,
+        rebounds: 12,
+        assists: 12,
+        steals: 3,
+        blocks: 1,
+        slam_dunks: 1
       }]
     },
     :away=>{
