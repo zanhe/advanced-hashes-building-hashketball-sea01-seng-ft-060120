fd2a5cbdbef9b808bd2c65df39e7777c30fe8dca
Zana H <zhertica@gmail.com>
2020-05-29 01:58:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,8 +1,8 @@
 def game_hash 
   {
     :home=>{
-      :team_name=>"Brooklyn Nets",
-      :colors=>[Black, White],
+      :team_name=>"",
+      :colors=>[],
       :players=>[{}]
     },
     :away=>{}
