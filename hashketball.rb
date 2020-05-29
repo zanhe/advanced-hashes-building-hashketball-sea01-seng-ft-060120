2842db5e084ece45224b3a947e5c3f97818bf15c
Zana H <zhertica@gmail.com>
2020-05-29 02:19:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -28,7 +28,7 @@ def game_hash
          player_name: "Ben Gordon", number: 8, shoe: 15, points: 33, rebounds: 3, assists: 2, steals: 1, blocks: 1, slam_dunks: 0
          },{
         player_name: "Kemba Walker", number: 33, shoe: 15, points: 6, rebounds: 12, assists: 12, steals: 7, blocks: 5, slam_dunks: 12
-      }]
+         }]
     }
   }
 end
