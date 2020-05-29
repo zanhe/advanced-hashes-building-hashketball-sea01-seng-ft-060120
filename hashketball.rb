dfa5473756e7f02da144a6993b6543624da279d9
Zana H <zhertica@gmail.com>
2020-05-29 02:10:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -8,7 +8,7 @@ def game_hash
       },{
         player_name: "Reggie Evans", number: 30, shoe: 14, points: 12, rebounds: 12, assists: 12, steals: 12, blocks: 12, slam_dunks: 7
       },{
-        player_name: "Brook Lopez", number: 11, shoe: 17, points: 17, rebounds: 19, assists: 10, steals: 3, blocks: 1, slam_dunks: 1
+        player_name: "Brook Lopez", number: 11, shoe: 17, points: 17, rebounds: 19, assists: 10, steals: 3, blocks: 1, slam_dunks: 15
       },
         
         
