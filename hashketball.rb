47da849314fa618ca018a68c0635b0dd0232d96b
Zana H <zhertica@gmail.com>
2020-05-29 02:16:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,7 +2,7 @@ def game_hash
   {
     :home=>{
       :team_name=>"",
-      :colors=>[],
+      :colors=>[Black,White],
       :players=>[{
         player_name: "Alan Anderson", number: 0, shoe: 16, points: 22, rebounds: 12, assists: 12, steals: 3, blocks: 1, slam_dunks: 1
       },{
@@ -17,8 +17,19 @@ def game_hash
     },
     :away=>{
       :team_name=>"Charlotte Hornets",
-      :colors=>[Tur],
-      :players=>[{}]
+      :colors=>[Turquoise,Purple],
+      :players=>[{
+         player_name: "Jeff Adrien", number: 4, shoe: 18, points: 10, rebounds: 1, assists: 1, steals: 2, blocks: 7, slam_dunks: 2
+      },{
+         player_name: "Bismack Biyombo", number: 0, shoe: 16, points: 12, rebounds: 4, assists: 7, steals: 22, blocks: 15, slam_dunks: 10
+      },{
+         player_name: "DeSagna Diop", number: 2, shoe: 14, points: 24, rebounds: 12, assists: 12, steals: 4, blocks: 5, slam_dunks: 5
+      }, {
+         player_name: "Ben Gordon", number: 8, shoe: 15, points: 33, rebounds: 3, assists: 2, steals: 1, blocks: 1, slam_dunks: 0
+      },{
+        player_name: "Kemba Walker", number: 33, shoe: 15, points: 6, rebounds: 12, assists: 12, steals: 7, blocks: 5, slam_dunks: 12
+        
+      }]
     }
   }
 end
