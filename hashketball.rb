876a1c30f372969f93dd2dfee89bb06af67e771b
Zana H <zhertica@gmail.com>
2020-05-29 02:02:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,16 @@ def game_hash
     :home=>{
       :team_name=>"",
       :colors=>[],
-      :players=>[{}]
+      :players=>[{:player_name=>{}
+        :number
+        :shoe
+        :points
+        :rebounds
+        :assists
+        :steals
+        :blocks
+        :slam_dunks
+      }]
     },
     :away=>{
       :team_name=>"",
