153f21fe9b4c1dea972937f77624f44dd5f833ef
Zana H <zhertica@gmail.com>
2020-05-29 02:06:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,16 +4,16 @@ def game_hash
       :team_name=>"",
       :colors=>[],
       :players=>[{
-        player_name: "Alan Anderson",
-        number: 0,
-        shoe: 16,
-        points: 22,
-        rebounds: 12,
-        assists: 12,
-        steals: 3,
-        blocks: 1,
-        slam_dunks: 1
-      }]
+        player_name: "Alan Anderson", number: 0, shoe: 16, points: 22, rebounds: 12, assists: 12, steals: 3, blocks: 1, slam_dunks: 1
+      },
+      player_name: "Reggie Evans", number:
+        shoe:
+        points:
+        rebounds:
+        assists:
+        steals:
+        blocks:
+        slam_dunks:]
     },
     :away=>{
       :team_name=>"",
